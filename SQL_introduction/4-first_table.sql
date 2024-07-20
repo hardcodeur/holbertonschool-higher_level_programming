cbe142e4eda5f81a9e6053b8cded9cbca60bf9bd
hardcodeur <vardump_0101@protonmail.com>
2024-07-20 09:56:06 +0000

    bouh

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
-#-- 3 first students in the Batch ID=3
-#-- because Batch 3 is the best!
-USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table
-(
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
-    nom VARCHAR(256),
-)
+    name VARCHAR(256)
+);
