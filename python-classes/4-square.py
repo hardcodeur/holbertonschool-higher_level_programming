7121287a78902e1693f1f684736ff04e56e84d03
hardcodeur <vardump_0101@protonmail.com>
2024-07-22 08:23:00 +0000

    bouh

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,29 +4,25 @@
 
 class Square:
     """Class that defines a square."""
-
-
     def __init__(self, size=0):
         """Initialize the square with an option"""
-
-
         self.size = size
 
     @property
     def size(self):
         """ Get the size of the square."""
-        
-
         return self.__size
     
     
     @size.setter
     def size(self, value):
         """Sets the size of the square."""
-        
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Computes the area of the square"""
+        return self.__size ** 2
