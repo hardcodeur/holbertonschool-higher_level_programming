db2c557d85e7d8f78eb426d347c6b7b41ec47ea7
bbel <bweb33@protonmail.com>
2024-07-26 09:51:22 +0200

    bouh

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@ const request = require('request'),movieId = process.argv[2];
 request
   .get("https://swapi-api.hbtn.io/api/films/"+movieId)
   .on('response', function(response) {
-    console.log("code: "+response.title)
+    console.log(response.title);
   })
