3dee32226f5f45712f1323e07757f9aa0ba7ef4a
bbel <bweb33@protonmail.com>
2024-07-26 14:50:57 +0200

    bouh

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const request = require('request'),url = process.argv[2],id = process.argv[3];
+const request = require('request'),url = process.argv[2];
 request(url, { json: true },function (error, response, body) {
   const movies = body.results;
   const characterId=id;
   let cpt = 0;
   movies.forEach(movie => {
     const characters = movie.characters;
-    const  charactersUrl = `https://swapi-api.hbtn.io/api/people/${characterId}/`;
+    const  charactersUrl = `https://swapi-api.hbtn.io/api/people/18/`;
     if(characters.includes(charactersUrl)){
       cpt++;
     }
